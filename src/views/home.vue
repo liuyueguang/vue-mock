80827c71aade9835cc56b6004a80f3383935182d
liuyueguang <liuyg8691@qq.com>
2021-06-29 16:38:51 +0800

    Add files via upload

---
+++ src/views/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>mockjs实现对虚拟数据的增删改查</h1>
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
